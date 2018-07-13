7a8f30b770a570cf29154ca9553ab44bd7b212fc
root <root@localhost.localdomain>
2018-07-13 12:59:09 -0400

    version 1.7.5

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-azure-plugin',
-    version='1.7.4',
+    version='1.7.5',
     license='LICENSE',
     packages=[
         'cloudify_azure',
